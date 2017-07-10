671272c55fcb81de81291a94b1ff486f8c19bd00
Unknown <abvaden801@gmail.com>
2017-07-10 07:58:43 -0400

    Cleaned up some unit test code around the select meal fulfillment class

--- test/bot-fulfillment/select-meal-fufillment.spec.js
+++ test/bot-fulfillment/select-meal-fufillment.spec.js
@@ -35,7 +35,14 @@ describe('SelectMealRequest', () => {
         var selectMealRequest = new SelectMealRequest();
         selectMealRequest.Parse(request);
 
-        expect(selectMealRequest.getLunch()).to.equal(true, 'Failed to parse lunch');
+        expect(selectMealRequest.getBreakfast()).to.equal(false, 'Failed to set true for breakfast in all request');
+        expect(selectMealRequest.getLunch()).to.equal(true, 'Failed to set true for lunch in all request');
+        expect(selectMealRequest.getSnack()).to.equal(false, 'Failed to set true for snack in all request');
+        expect(selectMealRequest.getDinner()).to.equal(false, 'Failed to set true for dinner in all request');
+        expect(selectMealRequest.getAny()).to.equal(false, 'Failed to set false for any in all request');
+
+        const validationErrors = selectMealRequest.Validate();
+        expect(validationErrors).to.be.empty;
     });
 
     it('Should properly parse a valid all meals request', () => {
@@ -73,5 +80,42 @@ describe('SelectMealRequest', () => {
         expect(selectMealRequest.getSnack()).to.equal(true, 'Failed to set true for snack in all request');
         expect(selectMealRequest.getDinner()).to.equal(true, 'Failed to set true for dinner in all request');
         expect(selectMealRequest.getAny()).to.equal(false, 'Failed to set false for any in all request');
+
+        const validationErrors = selectMealRequest.Validate();
+        expect(validationErrors).to.be.empty;
+    });
+
+    it('Should raise a validation error for an empty request', () => {
+        const request = {
+            "messageVersion": "1.0",
+            "invocationSource": "FulfillmentCodeHook",
+            "userId": "Some long string",
+            "sessionAttributes": null,
+            "bot": {
+                "name": "SummerFood",
+                "alias": null,
+                "version": "$LATEST"
+            },
+            "outputDialogMode": "Text",
+            "currentIntent": {
+                "name": "MealSelect",
+                "slots": {
+                    "all": null,
+                    "lunch": null,
+                    "snack": null,
+                    "breakfast": null,
+                    "any": null,
+                    "dinner": null
+                },
+                "confirmationStatus": "None"
+            },
+            "inputTranscript": "Find places that serve food"
+        };
+
+        var selectMealRequest = new SelectMealRequest();
+        selectMealRequest.Parse(request);
+
+        const validationErrors = selectMealRequest.Validate();
+        expect(validationErrors).to.be.not.empty;
     });
 });
